aca96253400b0c96e6fff5cd314fc570b72bf043
Mahima Srikanta <mahima.srikanta@gmail.com>
2017-05-20 10:42:41 -0700

    User tweets displayed

--- react-client/src/components/PublicTweets.jsx
+++ react-client/src/components/PublicTweets.jsx
@@ -22,7 +22,6 @@ class PublicTweets extends React.Component {
         favorite_count: 'XXX',
         retweet_count: 'XXX',
         entities: {
-          media: [],
           urls: [],
           user_mentions: [],
           hashtags: [],
@@ -37,22 +36,43 @@ class PublicTweets extends React.Component {
       if (!res.data) {
         console.log('error');
       }
+      console.log(res.data.statuses);
       this.setState({allTweets: res.data.statuses});
-    });
-    
-    
-  }
+    });       
+  } 
 
-  
   render() {
     console.log(this.props.tweets);
+    let formattedTweets = [];
     return (
       <div>
         <Header />
-          <h1> Hello from PublicTweets Page</h1>
-      </div>
-    );
+         {formattedTweets = this.state.allTweets.map((tweet, i) => {                
+           var formattedObject = {
+             'id': tweet.id,
+             'user': {
+               'name': tweet.user.name,
+               'screen_name': tweet.user.screen_name,
+               'profile_image_url': tweet.user.profile_image_url,
+             },
+             'text': tweet.text,
+             'created_at': tweet.created_at,
+             'favorite_count': tweet.favorite_count,
+             'retweet_count': tweet.retweet_count,
+             'entities': {
+               'urls': tweet.entities.urls,
+               'user_mentions': tweet.entities.user_mentions,
+               'hashtags': tweet.entities.hashtags,
+               'symbols': tweet.entities.symbols,
+             }
+           };
+           return <Tweet data={formattedObject} key={i} />;
+         })}
+         </div>         
+    );     
   }
+   
+
 
 }
 export default PublicTweets;
